4fca7b090a8b5a6acbd8af22344c1bb8c7366bb0
CountryMartin <D287805@edu.curio.nl>
2024-03-04 14:01:58 +0100

    Begin van de project crud

--- database/seeders/UserProjectSeeder.php
+++ database/seeders/UserProjectSeeder.php
@@ -12,6 +12,31 @@ class UserProjectSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
+    {        
+        DB::table('user_projects')->insert([
+            'user_id' => '1',
+            'project_id' => '1'
+        ]);
+
+        DB::table('user_projects')->insert([
+            'user_id' => '1',
+            'project_id' => '2'
+        ]);
+
+        DB::table('user_projects')->insert([
+            'user_id' => '1',
+            'project_id' => '3'
+        ]);
+
+        DB::table('user_projects')->insert([
+            'user_id' => '1',
+            'project_id' => '4'
+        ]);
+
+        DB::table('user_projects')->insert([
+            'user_id' => '1',
+            'project_id' => '5'
+        ]);
+
     }
 }
